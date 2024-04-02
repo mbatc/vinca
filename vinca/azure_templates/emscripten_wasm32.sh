25321201a9469e15289991cee48c83ad0f2666e8
Michael Batchelor <mickbatch98@gmail.com>
2024-04-03 00:03:19 +1000

    Dont install emsdk in script. instead handle it in ros-humble repo

--- vinca/azure_templates/emscripten_wasm32.sh
+++ vinca/azure_templates/emscripten_wasm32.sh
@@ -1,22 +1,3 @@
-
-echo "Setup emscripten-forge recipes"
-
-mkdir -p emforge-recipes
-pushd emforge-recipes
-echo "Clone https://github.com/emscripten-forge/recipes.git to $(cd)"
-git clone https://github.com/emscripten-forge/recipes.git .
-
-echo "-- Setup emsdk"
-./emsdk/setup_emsdk.sh "3.1.45" $(pwd)/emsdk_install
-
-echo "-- Install playwright"
-playwright install
-
-echo "-- Install custom boa"
-python -m pip install git+https://github.com/DerThorsten/boa.git@python_api_v2 --no-deps --ignore-installed
-popd
-
-echo "Build robostack"
 export CI=azure
 export GIT_BRANCH=$BUILD_SOURCEBRANCHNAME
 export FEEDSTOCK_NAME=$(basename ${BUILD_REPOSITORY_NAME})
